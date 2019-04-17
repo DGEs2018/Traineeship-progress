c60eb27439d3dd276b7e13721f3621c8d7922298
Dawit <dawit@ginetta.net>
2019-04-17 16:49:31 +0200

    Push the input array of strings to the empty array stored as uniqueEl variable

---
+++ Week3/TIWD-Project/unique-element.js
@@ -0,0 +1,36 @@
+// EXERCISE 3
+// Restore the comments marks (/* */) of the previous exercises.
+// Uncomment the exercise code deleting /* and */ chars
+// Create a function that takes an array of string and returns the unique values
+
+
+/* const elements = ['hi', 'hello', 'hi', 'hello world!', 'hola', 'hallo', 'hola'];
+const unique = function() {}
+const uniqueElements = unique(elements);
+console.log(uniqueElements) // ['hi', 'hello', 'hello world!', 'hola', 'hallo']
+ */
+const words = ['hi', 'hello', 'hi', 'hello world!', 'hola', 'hallo', 'hola'];
+// const unique = function(elements) {
+//     const uniqueElements = [];
+//     for (let i = 0; i < elements.length; i++) {
+//         if (elements[i] = 1){
+//             elements[i] = uniqueElements;
+//         } 
+//         return uniqueElements
+//     }
+// }
+
+function uniqueElements(elements) {
+    const uniqueEl = [];
+    console.log(uniqueEl);
+    for(let i = 0; i < elements.length; i++) {
+        console.log(elements);
+
+    }
+    return uniqueEl;
+    uniqueEl === 
+}
+
+console.log(uniqueElements(words));
+
+
