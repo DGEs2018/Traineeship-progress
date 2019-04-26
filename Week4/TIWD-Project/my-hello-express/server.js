829244486a8738f4746e40b856b81af2463ac40f
Dawit <dawit@ginetta.net>
2019-04-26 15:06:23 +0200

    Send a GET request to the server

--- Week4/TIWD-Project/my-hello-express/server.js
+++ Week4/TIWD-Project/my-hello-express/server.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 var port = 3000;
+//When request was sent before this block of code, with the localhost:3000 in the address bar of the browser but ends up in error b/c, instructions on how to handle tbe request are missing
+app.get('/', function(request, response) {
+	response.send('Hello, World!');
+});
 
+// using the Express listen function to command the server to start listening, passing in the port variable for the port number
 app.listen(port, function() {
-	console.log('Express app listening on port ' + port);
+	console.log('Express app listening on port ' + port); // is to communicating to tell the server to start listening requests on the specified port
 });
