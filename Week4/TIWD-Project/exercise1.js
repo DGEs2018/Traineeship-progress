36c43f10fa189f0aa7d8ab0643941e9fedc81d2b
Dawit <dawit@ginetta.net>
2019-04-26 13:56:56 +0200

    Review 1st exercise

--- Week4/TIWD-Project/exercise1.js
+++ Week4/TIWD-Project/exercise1.js
@@ -4,9 +4,19 @@
 // Uncomment the exercise code deleting /* and */ chars.
 // Write a function that takes a string and returns the longest word.
 
-/*
 const sentence1 = 'guide your learning through pain';
-const findLongest = function() {}
-const longestWord = findLongest(sentence1);
-console.log(longestWord === 'learning'); // true
-*/
+// const findLongest = function() {}
+const splittedSent = sentence1.split(' ').join();
+
+const longestWord = function(sentence1) {
+	for (let i = 0; i < splittedSent.length; i++) {
+		// create a variable to assign the temporarily longest word
+		let forNowLongest = splittedSent[0];
+		// compare the temp. longest word to the following ones
+		if (forNowLongest <= splittedSent[i].length) {
+			forNowLongest = splittedSent[i];
+		}
+	}
+	return forNowLongest;
+};
+console.log(longestWord(sentence1)); // true
