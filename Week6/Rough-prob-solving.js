6a7992dff115a7f1f60f2f3c950d2673cfd2d258
Dawit <dawit@ginetta.net>
2019-05-06 10:18:56 +0200

    Start journal of Week6

---
+++ Week6/Rough-prob-solving.js
@@ -0,0 +1,17 @@
+9. Two Sum
+Write a method that takes an array of numbers. If a pair of numbers in the array sums to zero, return the positions of those two numbers. If no pair of numbers sums to zero, return null.
+function two_sum(nums) {}
+
+two_sum([1, 3, -1, 5]); // [[0, 2]]
+two_sum([1, 3, -1, 5, -3]); // [[0, 2], [1, 4]]
+two_sum([1, 5, 3, -4]); // null
+
+function nullSumNum(nums) {
+ // Find out the sum of each of the characters in the array
+ for (let i = 0; i < sums.length; i++) {
+     // If the sum of any two characters inside returns zero
+   // return these in an array of two
+
+ }
+ 
+}
---
+++ Week6/journal.md
@@ -0,0 +1,7 @@
+* 06.05.2019
+- SPAs
+- What they are ?
+    - Are special kind of websites where the user doesn't see any reload despite interacting with the page.
+    - SPA relies primarily on JS - interaction with server to change the content in the page.
+    - Usually heavy on the first load as this requires the browser to download the entire content.
+    - 
