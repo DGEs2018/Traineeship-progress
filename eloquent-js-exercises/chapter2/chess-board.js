034a18331896f3d845d02b6d8a9c3a99ced27aa9
Dawit <dawit@ginetta.net>
2019-05-13 15:36:22 +0200

    Remove directory after suggestion

--- eloquent-js-exercises/chapter2/chess-board.js
+++
@@ -1,8 +0,0 @@
-let input = '####';
-
-function chessBoard(input) {
-	for (let input = '####'; input.length < 5; input++) {
-		console.log("input /n''input");
-	}
-	return input;
-}
--- eloquent-js-exercises/chapter2/triangle-looper.js
+++
@@ -1,7 +0,0 @@
-function trianglePrinter () {
-    for (let input = '#'; input.length < 8; input +=) {
-        console.log(input);
-    }
-}
-
-console.log(trianglePrinter('#'));
