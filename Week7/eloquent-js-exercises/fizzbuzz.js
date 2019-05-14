8e4f6c9191a5d9a963b98ad5a1810abed7321dee
Dawit <dawit@ginetta.net>
2019-05-14 13:53:26 +0200

    Describe the problem in your own words

--- Week7/eloquent-js-exercises/fizzbuzz.js
+++ Week7/eloquent-js-exercises/fizzbuzz.js
@@ -1,8 +1,8 @@
-/*Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
-For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), 
-print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz" for numbers that 
-are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
-*/
+/*
+Write a program that prints all the numbers 1 to 100, instead of the numbers themselves,
+for numbers divisible by 3 "Fizz", divisible by "Buzz", divisible by both "FizzBuzz", 
+whereas for those that don't fulfill the above conditions, just the number itself.
+ */
 
 function fizzBuzz(num) {
 	for (let num = 0; num < 100; num++) {
