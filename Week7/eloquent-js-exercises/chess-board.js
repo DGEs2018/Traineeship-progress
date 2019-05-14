01475bc8142dfee185197eb2d42044936aa97a17
Dawit <dawit@ginetta.net>
2019-05-14 09:20:27 +0200

    Remove the eloquent-js-exercises directory parallel to the weeks directory

--- Week7/eloquent-js-exercises/chess-board.js
+++ Week7/eloquent-js-exercises/chess-board.js
@@ -1,14 +1,23 @@
 /*
  Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
  */
-let string = '####';
+let output = ' ';
 function chessBoard(string) {
-
-for (let string = 1;  < 9; counter++ ){
-	string = string + 1;
+	let counter = 1;
+	for (let counter = 1; counter < 5; counter += '#') {
+		// need to work my head around to have the counters, might be too early for i
+		// counter+= "####";
+		for (let counter = 1; counter < 9; counter += '#') {
+			if (counter % 2 !== 0) {
+				console.log(' ####');
+			} else if (counter % 2 === 0) {
+				console.log('#### ');
+			}
+		}
 	}
+	return counter;
 }
-console.log(string + " \n" string);
+console.log(output);
 
 /*
 Passing this string to console.log should show something like this:
--- eloquent-js-exercises/chapter2/chess-board.js
+++
@@ -1,6 +0,0 @@
-let string = '####';
-
-for (let string = 1; counter < 9; counter++ ){
-	string = string + 1;
-}
-console.log(string + " \n" string);
