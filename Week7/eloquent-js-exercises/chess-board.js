2b500bb1e72ad65dfa61d6cc766950e705d625cf
Dawit <dawit@ginetta.net>
2019-05-14 13:17:28 +0200

    Solve chess-board exercise

--- Week7/eloquent-js-exercises/chess-board.js
+++ Week7/eloquent-js-exercises/chess-board.js
@@ -1,25 +1,32 @@
 /*
  Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
  */
-let output = ' ';
-function chessBoard(string) {
-	let counter = 1;
-	for (let counter = 1; counter < 5; counter += '#') {
+// let output = ' ';
+function chessBoard(white, dark) {
+	let white = ' ';
+	let dark = '#';
+
+	for (let i = 0; i < 8; i++) {
 		// need to get my head around to have the counters, could that be done without indices
+		if (i % 2 === 0) {
+			console.log( white);
+		} else(i % !== 0) {
+			console.log(dark);
+		}
 		// this will be for the left to right going #s
 		// counter+= "####";
-		for (let counter = 1; counter < 9; counter += '#') {
-			//for the top to bottom going #s
-			/* if (counter % 2 !== 0) {
-				console.log(' ####');
-			} else if (counter % 2 === 0) {
-				console.log('#### '); */
+		for (let j = 0; j < 8; j++) {
+			if (i % 2 === 0) {
+				console.log( white);
+			} else(i % !== 0) {
+				console.log(dark);
+			}
 			}
 		}
 	}
-	return counter;
-}
-console.log(output);
+	return chessBoard;
+
+console.log(chessBoard(white, dark));
 
 /*
 Passing this string to console.log should show something like this:
