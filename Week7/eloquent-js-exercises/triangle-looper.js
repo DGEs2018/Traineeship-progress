aca299c48c00db53bfd8b5f286ac6f20355bfc28
Dawit <dawit@ginetta.net>
2019-05-14 07:28:24 +0200

    Fix incremental input plus give in parameter to function

--- Week7/eloquent-js-exercises/triangle-looper.js
+++ Week7/eloquent-js-exercises/triangle-looper.js
@@ -3,13 +3,13 @@ Write a loop that makes seven calls to console.log to output the following trian
 
 */
 
-function trianglePrinter () {
-    for (let input = '#'; input.length < 8; input +=) {
-        console.log(input);
-    }
+function trianglePrinter(input) {
+	for (let input = '#'; input.length < 8; input += '#') {
+		console.log(input);
+	}
 }
 
-console.log(trianglePrinter('#'));
+console.log(trianglePrinter(input));
 
 /* 
 #
