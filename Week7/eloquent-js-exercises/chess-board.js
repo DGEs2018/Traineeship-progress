99bc708e50c96d0f040166bcb0ce64c7dd6969b2
Dawit <dawit@ginetta.net>
2019-05-15 15:04:59 +0200

    Encapsulate the chess problem in a function

--- Week7/eloquent-js-exercises/chess-board.js
+++ Week7/eloquent-js-exercises/chess-board.js
@@ -2,19 +2,21 @@
  Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. 
  At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
  */
-
-let chessBoard = '';
-for (let j = 0; j < 8; j++) {
-	for (let i = 0; i < 8; i++) {
-		if ((i + j) % 2 === 0) {
-			chessBoard += ' ';
-		} else {
-			chessBoard += '#';
+function chessBoardMaker() {
+	let chessBoard = '';
+	for (let j = 0; j < 8; j++) {
+		for (let i = 0; i < 8; i++) {
+			if ((i + j) % 2 === 0) {
+				chessBoard += ' ';
+			} else {
+				chessBoard += '#';
+			}
 		}
+		chessBoard += '\n';
 	}
-	chessBoard += '\n';
+	return chessBoard;
 }
-console.log(chessBoard);
+console.log(chessBoardMaker());
 
 /*
 expected output
@@ -33,20 +35,23 @@ expected output
 /* for checkerboard of all size grids, stored as a variable named size, where instead of changing the grid 
 size manually it could be replaced by varying the variable size*/
 
-let size = 14;
-let chessBoard = '';
-for (let j = 0; j < size; j++) {
-	for (let i = 0; i < size; i++) {
-		if ((i + j) % 2 === 0) {
-			chessBoard += ' ';
-		} else {
-			chessBoard += '#';
+function chessBoardMaker() {
+	let size = 14;
+	let chessBoard = '';
+	for (let j = 0; j < size; j++) {
+		for (let i = 0; i < size; i++) {
+			if ((i + j) % 2 === 0) {
+				chessBoard += ' ';
+			} else {
+				chessBoard += '#';
+			}
 		}
+		chessBoard += '\n';
 	}
-	chessBoard += '\n';
+	return chessBoard;
 }
-console.log(chessBoard);
-
+console.log(chessBoardMaker);
+ */
 /*
 should return what's shown below
 
