a4fe872d8d069d5b289abffcc2d4b75df192fe11
Dawit <dawit@ginetta.net>
2019-05-15 14:18:10 +0200

    Complete triangle problem with random size generator

--- Week7/eloquent-js-exercises/triangle-looper.js
+++ Week7/eloquent-js-exercises/triangle-looper.js
@@ -3,14 +3,14 @@ Write a triangle that outputs a right angle triangle,
 starting with 1 # at the left topmost to 7 #s at the right bottom
 
 */
-let input = '#';
+/* let input = '#';
 function trianglePrinter(input) {
 	for (let input = '#'; input.length < 8; input += '#') {
 		console.log(input);
 	}
 }
 
-console.log(trianglePrinter(input));
+console.log(trianglePrinter(input)); */
 
 /* 
 Expected output should be similar to the display below.
@@ -21,3 +21,20 @@ Expected output should be similar to the display below.
 #####
 ######
 ####### */
+// the variety with automatic size triangle generator
+const height = 4;
+const input = '#';
+function trianglePrinter() {
+	for (let input = '#'; input.length < height + 1; input += '#') {
+		console.log(input);
+	}
+}
+
+console.log(trianglePrinter(height));
+
+/*
+Should return this
+#
+##
+###
+#### */
