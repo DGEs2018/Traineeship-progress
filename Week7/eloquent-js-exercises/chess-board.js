0cce7388fbacd12845e5dcb4b2cab09dc848df47
Dawit <dawit@ginetta.net>
2019-05-15 10:48:56 +0200

    Solve both versions of the checker-board problem

--- Week7/eloquent-js-exercises/chess-board.js
+++ Week7/eloquent-js-exercises/chess-board.js
@@ -3,67 +3,6 @@
  At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
  */
 
-// let white = ' ';
-// let dark = '#';
-
-// function chessBoard(white, dark) {
-// 	for (let i = 0; i < 8; i++) {
-// 		// horizontals / rows
-// 		if (i % 2 === 0) {
-// 			console.log(white);
-// 		} else if (i % 2 !== 0) {
-// 			console.log(dark);
-// 		}
-// 		// replicate this for loop for the rest of the rows
-
-// 		// console.log("\n");
-
-// 		// verticals / columns
-// 		for (let j = 0; j < 8; j++) {
-// 			if (j % 2 === 0) {
-// 				console.log(white);
-// 			} else if (j % 2 !== 0) {
-// 				console.log(dark);
-// 			}
-// 		}
-// 	}
-// 	// replicate this for loop for the rest of the columns
-// 	return chessBoard;
-// }
-
-/* let chessBoard = '';
-let dark = '#';
-let white = ' ';
-// iterate over each of the 8 rows, top to bottom
-for (let j = 0; j < 8; j++) {
-	// once the loop goes all the way through the first row till 8th position it should jump to the next newline
-	if (j > 0) chessBoard += '\n';
-	// in each row it should populate the inputs vertically
-	for (let i = 0; i < 8; i++) {
-		// where the sum of the ith and jth position is even populate the checkboard with white
-		if ((i + j) % 2 === 0) {
-			chessBoard += white;
-			// else where the sum of the ith and jth position is odd populate the checkboard with dark
-		} else {
-			chessBoard += dark;
-		}
-	}
-}
-
-chessBoard; */
-/*
-Passing this string to console.log should show something like this:
-
- # # # #
-# # # # 
- # # # #
-# # # # 
- # # # #
-# # # # 
- # # # #
-# # # #
- */
-
 let chessBoard = '';
 for (let j = 0; j < 8; j++) {
 	for (let i = 0; i < 8; i++) {
