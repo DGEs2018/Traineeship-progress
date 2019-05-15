5a020d2c8bde7a3517ae43d82ca2a539f339ba1f
Dawit <dawit@ginetta.net>
2019-05-15 08:50:28 +0200

    Solve both versions of the checker-board problem

--- Week7/eloquent-js-exercises/chess-board.js
+++ Week7/eloquent-js-exercises/chess-board.js
@@ -63,16 +63,66 @@ Passing this string to console.log should show something like this:
  # # # #
 # # # #
  */
-let size = 12;
-let board = '';
+
+let chessBoard = '';
+for (let j = 0; j < 8; j++) {
+	for (let i = 0; i < 8; i++) {
+		if ((i + j) % 2 === 0) {
+			chessBoard += ' ';
+		} else {
+			chessBoard += '#';
+		}
+	}
+	chessBoard += '\n';
+}
+console.log(chessBoard);
+
+/*
+expected output
+
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # #
+
+ */
+
+/* for checkerboard of all size grids, stored as a variable named size, where instead of changing the grid 
+size manually it could be replaced by varying the variable size*/
+
+let size = 14;
+let chessBoard = '';
 for (let j = 0; j < size; j++) {
 	for (let i = 0; i < size; i++) {
 		if ((i + j) % 2 === 0) {
-			board += ' ';
+			chessBoard += ' ';
 		} else {
-			board += '#';
+			chessBoard += '#';
 		}
 	}
-	board += '\n';
+	chessBoard += '\n';
 }
-console.log(board);
+console.log(chessBoard);
+
+/*
+should return what's shown below
+
+ # # # # # # #
+# # # # # # # 
+ # # # # # # #
+# # # # # # # 
+ # # # # # # #
+# # # # # # # 
+ # # # # # # #
+# # # # # # # 
+ # # # # # # #
+# # # # # # # 
+ # # # # # # #
+# # # # # # # 
+ # # # # # # #
+# # # # # # # 
+ */
