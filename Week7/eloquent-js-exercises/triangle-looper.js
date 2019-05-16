913969be7ca7914e2fc5abf53077496a97f47de3
Dawit <dawit@ginetta.net>
2019-05-16 10:14:41 +0200

    Make use of the defined variable through the function

--- Week7/eloquent-js-exercises/triangle-looper.js
+++ Week7/eloquent-js-exercises/triangle-looper.js
@@ -3,14 +3,15 @@ Write a triangle that outputs a right angle triangle,
 starting with 1 # at the left topmost to 7 #s at the right bottom
 
 */
-/* let input = '#';
+
 function trianglePrinter(input) {
+	let input = '#';
 	for (let input = '#'; input.length < 8; input += '#') {
 		console.log(input);
 	}
 }
 
-console.log(trianglePrinter(input)); */
+trianglePrinter(input);
 
 /* 
 Expected output should be similar to the display below.
@@ -22,15 +23,15 @@ Expected output should be similar to the display below.
 ######
 ####### */
 // the variety with automatic size triangle generator
-const height = 4;
-const input = '#';
-function trianglePrinter() {
+
+function trianglePrinter(height) {
+	const input = '#';
 	for (let input = '#'; input.length < height + 1; input += '#') {
 		console.log(input);
 	}
 }
 
-console.log(trianglePrinter(height));
+console.log(trianglePrinter(4));
 
 /*
 Should return this
