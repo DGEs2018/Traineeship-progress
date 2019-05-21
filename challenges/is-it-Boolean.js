5cfbf70e7ce2da140c43a7eace5962390eb65fee
Dawit <dawit@ginetta.net>
2019-05-21 17:26:13 +0200

    Modify the function to return the value to requested true or false values using return

--- challenges/is-it-Boolean.js
+++ challenges/is-it-Boolean.js
@@ -1,11 +1,14 @@
+/*
+Write a function that checks if a given value is Boolean or not and returns true or false.
+*/
 function isItBoolean(value) {
 	if (value === true || value === false) {
-		console.log(true + ", it's indeed a Boolean");
+		return true;
 	} else {
-		console.log(false);
+		return false;
 	}
-	return value;
 }
 
-isItBoolean(true); // should return "true, it's indeed a Boolean"
-isItBoolean(10); // should return 'false'
+isItBoolean(true); // should return true
+isItBoolean(false); // should return true
+isItBoolean(10); // should return false
