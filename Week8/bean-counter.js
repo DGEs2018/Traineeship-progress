c22d47443dfb5bdada8407b929bb5b324394bcb7
Dawit <dawit@ginetta.net>
2019-05-22 11:18:18 +0200

    Use strict equality === as opposed to ==

--- Week8/bean-counter.js
+++ Week8/bean-counter.js
@@ -5,7 +5,7 @@ string characters to be counted.
 function countChar(string, character) {
 	let charQuantity = 0;
 	for (let i = 0; i < string.length; i++) {
-		if (string[i] == character) {
+		if (string[i] === character) {
 			charQuantity += 1; // with return (charQuantity += 1), it counted repeated characters as single
 		}
 	}
