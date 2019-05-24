d4cc6dcf939876f58cbf960210bb14849d8f6289
Dawit <dawit@ginetta.net>
2019-05-24 11:19:10 +0200

    Respond to PR

--- challenges/factorial.js
+++ challenges/factorial.js
@@ -1,3 +1,8 @@
+/**
+ * Write a method that takes an integer n and returns its factorial: n*(n-1)*(n-2)*...*2\*1. 
+ * Assumption n >= 0.
+ * @param {*} num - number parameter
+ */
 function factorial(num) {
 	//initialize a value to hold the required output, which serves to succinctly update the multiplication
 	let output = 1;
@@ -10,16 +15,4 @@ function factorial(num) {
 console.log(factorial(5)); //should return 120
 console.log(factorial(7)); //should return 5040
 
-/* function factorial(num){
-    let multi = 1
-    while(factorial)
-    if (factorial == 0 || factorial == 1){
-        break;
-        result => result * factorial
-        factorial => num - 1
-    }
-    for ( num > 1; i < num-1; num**) {
-        return multi =* i 
-    }
-    console.log(multi);
- */
+console.assert(factorial(5) === 140, 'Not correct'); // as the first comparison is false (deliberately for demonstration), 'Assertion failed: Not correct' message is printed.
