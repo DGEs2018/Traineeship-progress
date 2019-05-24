c3f39db676150bdad165d6474fa38e41870d8922
Dawit <dawit@ginetta.net>
2019-05-24 11:42:10 +0200

    Respond to PR feedbacks

--- challenges/is-it-palindrome.js
+++ challenges/is-it-palindrome.js
@@ -1,12 +1,11 @@
-/*
-Write a method that takes a string and returns true if it is a palindrome. A palindrome is a string that is the same whether written backward or forward.
-
-Note: Don't forget that strings are case-sensitive.
-*/
-
+/**
+ * Write a method that takes a string and returns true if it is a palindrome. A palindrome is a string that is the same whether written backward or forward.
+ * NB. strings are case-sensitive.
+ * @param {*} word 
+ */
 function palindrome(word) {
 	//to take case-sensitivity into consideration, convert the input to lowercase beforehand
-	let lowerCaseWord = word.toLowerCase();
+	//const lowerCaseWord = word.toLowerCase();
 	// Define an empty string variable to use it collect characters after iteration
 	let characterHolder = '';
 	for (let i = word.length - 1; i >= 0; i--) {
@@ -15,7 +14,7 @@ function palindrome(word) {
 		//then concatenate each character one by one
 	}
 	//  now once the iteration is finished, compare the word composed, and if this is identical to the input word
-	if (lowerCaseWord === characterHolder) {
+	if (word === characterHolder) {
 		// then return true
 		return true;
 		// otherwise false
@@ -24,7 +23,7 @@ function palindrome(word) {
 	}
 }
 
-console.log(palindrome('anna')); // should return true
+console.log(palindrome('adda')); // should return true
 console.log(palindrome('Anna')); // should return false
 console.log(palindrome('abcd')); // should return false
 console.log(palindrome('this one')); // should return false
