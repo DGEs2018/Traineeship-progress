b194c3c7ee649309abda27ea21bf59beb74f07ef
Dawit <dawit@ginetta.net>
2019-05-27 16:45:56 +0200

    Wip the nested list problem

--- Week9/eloquent-js-chapter4-exercises/a-list.js
+++ Week9/eloquent-js-chapter4-exercises/a-list.js
@@ -2,9 +2,17 @@
  * Write a function that builds list: with two properties, each of which containing nested propertiees 
  */
 function arrayToList(array) {
-	let emptyList = [];
-	for (let i = 0; i < array.length - 1; i++) {
-		emptyList.push;
+	let arrayHolder = null;
+	for (let i = array.length - 1; i >= 0; i--) {
+		list = { value1: array[i], rest: list };
+	}
+	return arrayHolder;
+}
+
+function listToArray(list) {
+	let listContainer = [];
+	for (let i = array.length - 1; i >= 0; i--) {
+		listContainer.push(node.value);
 	}
 }
 
