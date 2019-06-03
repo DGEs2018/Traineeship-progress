d85aabdada89e0586e29f52e89318f581b7edd37
Dawit <dawit@ginetta.net>
2019-06-03 09:30:05 +0200

    Wip define parameter

--- Week9/eloquent-js-chapter4-exercises/list-w-loop.js
+++ Week9/eloquent-js-chapter4-exercises/list-w-loop.js
@@ -1,7 +1,14 @@
-//list problem
-function arrayToList(list) {
-	for (let i = list.length - 1; i >= 0; i--) {
-		if ((i = list.length - 1)) {
+/**
+ * Write a function that builds list: with two properties, each of which 
+ * containing nested properties 
+ * 
+ * @param {*} arr
+ */
+function arrayToList(arr) {
+	let list = null;
+	let array = [];
+	for (let i = arr.length - 1; i >= 0; i--) {
+		if ((i = arr.length - 1)) {
 			list = {
 				value: arr[i],
 				rest: null
@@ -14,5 +21,5 @@ function arrayToList(list) {
 		}
 	}
 }
-let list = null;
-console.log(list);
+
+console.log(arrayToList([ 1, 2, 3 ]));
