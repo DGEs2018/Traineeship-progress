de0217df0410751742ebd1d0ffee36e2004dece0
Dawit <dawit@ginetta.net>
2019-06-03 13:59:44 +0200

    Wip on the desired object

---
+++ challenges/split-any-bill.js
@@ -0,0 +1,25 @@
+/**
+ * Write a function showing the list of people, sharing a bill with 
+ * the corresponding amount of the bill. 
+ * @param {*} group 
+ */
+function splitAnyBill(group) {
+	// find the total amount paid // variable is converts an object to an array of arrays
+	const costInArray = Object.values(group);
+	console.log(costInArray);
+	const reducer = (accumulator, currentValue) => accumulator + currentValue;
+	const totalCost = costInArray.reduce(reducer);
+	console.log(totalCost);
+	// the amount is supposed to pay / cost/number people
+	const costPerHead = totalCost / costInArray.length;
+	console.log(costPerHead);
+	// use the difference to find out who has overpaid or needs to be compensated
+	for (const [ key, value ] of Object.entries(group)) {
+		return {
+			key: costPerHead - value
+		};
+	}
+}
+
+const group = { pers1: 400, pers2: 100, pers3: 150, pers4: 360 };
+console.assert(splitAnyBill(group), 'revise again'); //
