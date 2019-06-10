a84b92d5417f1044eb9840f7da6a6cc7ea917009
Dawit <dawit@ginetta.net>
2019-06-10 12:08:09 +0100

    Solve the vowel counter puzzle in two different ways

--- challenges/count-vowels.js
+++ challenges/count-vowels.js
@@ -22,4 +22,20 @@ function vowelCounter(string) {
 	// return the updated counter variable
 	return counter;
 }
-console.assert(vowelCounter('this statement must have so many vowels'), 'incorrect');
+console.log(vowelCounter2('this statement has so many vowels')); // should return 9
+console.assert(vowelCounter('this statement has so many vowels'), 'incorrect'); //should not print nothing if the program works, but returns 'assert failed: incorrect if there's an error
+
+// second way storing the vowels as an array and using the .includes method
+function vowelCounter2(input) {
+	const listOfVowels = [ 'a', 'e', 'i', 'o', 'u' ];
+	let counter2 = 0;
+	let lowerCaseInput = input.toLowerCase();
+	for (let element of lowerCaseInput) {
+		if (listOfVowels.includes(element)) {
+			counter2++;
+		}
+	}
+	return counter2;
+}
+console.log(vowelCounter2('checkout how many vowels are here!')); // should return 11
+console.assert(vowelCounter2('checkout how many vowels are here!'), 'incorrect'); //should not print nothing if the program works, but returns 'assert failed: incorrect if there's an error
