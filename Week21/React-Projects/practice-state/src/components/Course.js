1a20a48b599a35f1d1606fc541782bd7203b7768
Dawit <dawit@ginetta.net>
2019-08-27 10:33:43 +0200

    Modify React version updates in within the package.json file in the practice state folder for ReactJS

--- Week21/React-Projects/practice-state/src/components/Course.js
+++ Week21/React-Projects/practice-state/src/components/Course.js
@@ -2,18 +2,18 @@ import React from 'react';
 import StarRating from './StarRating';
 
 const Course = (props) => {
-  return (
-    <div className="card">
-      <div>
-        <img src={props.url} alt={props.name} />
-      </div>
-      <h2>{ props.name }</h2>
-      <p>{ props.desc }</p>
-      <h3>Course Rating</h3>
-      {/* A self-contained star rating component */}
-      <StarRating /> 
-    </div>
-  );
-}
+	return (
+		<div className="card">
+			<div>
+				<img src={props.url} alt={props.name} />
+			</div>
+			<h2>{props.name}</h2>
+			<p>{props.desc}</p>
+			<h3>Course Rating</h3>
+			{/* A self-contained star rating component */}
+			<StarRating />
+		</div>
+	);
+};
 
 export default Course;
--- Week21/React-Projects/practice-state/src/components/StarRating.js
+++ Week21/React-Projects/practice-state/src/components/StarRating.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Star from './Star';
 
-class StarRating extends Component {
+const StarRating = () => {
 	// Initialize a 'rating' state
-	state = {
-		rating: 0
-	};
-
+	const [ rating, setRating ] = useState(0);
 	// Write a function that returns 5 Star components
-	renderStars = () => {
+	const renderStars = () => {
 		let stars = [];
 		let biggestRating = 5;
 
+		/* stars.map((star) =>
+			stars.push(<Star rating={rating} index={i} cheese="heeloo" setRating={() => setRating(i + 1)} key={i} />)
+		); */
+
 		for (let i = 0; i < biggestRating; i++) {
-			stars.push(<Star setRating={() => this.handleSetRating(i + 1)} key={i} />);
+			stars.push(<Star rating={rating} index={i} cheese="heeloo" setRating={() => setRating(i + 1)} key={i} />);
 		}
 		return stars;
 	};
 
 	// Write an event handler that updates the rating state.
 	// Pass the function to a Star component via props
-	handleSetRating = (rating) => {
-		this.setState({ rating: rating });
-	};
-	render() {
-		return (
-			<ul className="course--stars">
-				{/* Render the Star components */}
-				{this.renderStars()}
-			</ul>
-		);
-	}
-}
+
+	return (
+		<ul className="course--stars">
+			{/* Render the Star components */}
+			{renderStars()}
+			{/* //map */}
+		</ul>
+	);
+};
 
 export default StarRating;
