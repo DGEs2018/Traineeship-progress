44f55c7293489de2f01cefc3397fa5cc0e307f5e
Dawit <dawit@ginetta.net>
2019-08-30 09:58:33 +0200

    Refactor into and get to work the for-loop with .map method in the StarRating.js file in the practice-state folder within the React-Projects folder

--- Week21/React-Projects/practice-state/src/components/StarRating.js
+++ Week21/React-Projects/practice-state/src/components/StarRating.js
@@ -9,15 +9,15 @@ const StarRating = () => {
 		let stars = [];
 		let biggestRating = 5;
 
-		/* stars.map((star) =>
-			stars.push(<Star rating={rating} index={i} cheese="heeloo" setRating={() => setRating(i + 1)} key={i} />)
-		); */
+		stars.map(rating =>
+			<Star key={i} rating={rating} index={i} setRating={() => setRating(i + 1)} />
+		); 
 
-		for (let i = 0; i < biggestRating; i++) {
+/* 		for (let i = 0; i < biggestRating; i++) {
 			stars.push(<Star rating={rating} index={i} cheese="heeloo" setRating={() => setRating(i + 1)} key={i} />);
 		}
 		return stars;
-	};
+	}; */
 
 	// Write an event handler that updates the rating state.
 	// Pass the function to a Star component via props
